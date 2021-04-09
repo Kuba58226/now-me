5853cbffdadae0c70a03b7a611b1e5e419a18cb0
Kuba58226 <kubaozut@gmail.com>
2021-04-09 23:19:02 +0200

    changed registration, admin role authorization

---
+++ server/app/Http/Middleware/EnsureRoleIsAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class EnsureRoleIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return response()->json(['error' => 'Unauthenticated'], 401);
+        }
+        if (auth()->user()->role !== 'admin')
+        {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $next($request);
+    }
+}

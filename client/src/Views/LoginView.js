2dc800ef5bd375607abb3873aaaee5dd2c2801e6
Cyzol <karol8.9@o2.pl>
2021-04-11 19:19:15 +0200

    login validation

--- client/src/Views/LoginView.js
+++ client/src/Views/LoginView.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import FormField from '../components/molecules/FormField/FormField';
 import styled from 'styled-components';
 import { Button } from '../components/atoms/Button/Button';
-import axios from 'axios';
 
 import { ReactComponent as ManLogo } from '../assets/images/Group.svg';
 import { ReactComponent as WomenLogo } from '../assets/images/Group-1.svg';
 
-import { useSelector, useDispatch } from 'react-redux';
-import { selectUserToken } from '../features/appSlice';
+import { useDispatch } from 'react-redux';
 import { enterUserToken } from '../features/appSlice';
 
 const initialFormState = {
@@ -17,9 +16,15 @@ const initialFormState = {
   password: '',
 };
 
+const initialValidState = {
+  email: true,
+  password: true,
+};
+
 const LoginView = () => {
   const [user, setUser] = useState([]);
   const [formValues, setFormValues] = useState(initialFormState);
+  const [isValid, setIsValid] = useState(initialValidState);
 
   const dispatch = useDispatch();
   const history = useHistory();
@@ -60,14 +65,44 @@ const LoginView = () => {
     setUser(newUser);
   };
 
+  const handleValidData = (e) => {
+    if (e.target.name === 'email')
+      if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(formValues.email)) {
+        setIsValid({
+          ...isValid,
+          [e.target.name]: true,
+        });
+      } else {
+        setIsValid({
+          ...isValid,
+          [e.target.name]: false,
+        });
+      }
+    else if (e.target.name === 'password') {
+      if (formValues.password.length < 8) {
+        setIsValid({
+          ...isValid,
+          [e.target.name]: false,
+        });
+      } else {
+        setIsValid({
+          ...isValid,
+          [e.target.name]: true,
+        });
+      }
+    }
+  };
+
   return (
     <Container>
       <Wrapper>
         <LeftSide>
           <h2>Now You !</h2>
           <Form as="form" onSubmit={handleSubmitRegister}>
-            <FormField label="Email" id="email" name="email" onChange={handleInputChange} />
-            <FormField label="Password" id="password" name="password" type="password" onChange={handleInputChange} />
+            <FormField label="Email" id="email" name="email" onChange={handleInputChange} onBlur={handleValidData} />
+            {isValid.email === false ? <p>Wrong email format</p> : null}
+            <FormField label="Password" id="password" name="password" type="password" onChange={handleInputChange} onBlur={handleValidData} />
+            {isValid.password === false ? <p>Password is too short </p> : null}
             <Button type="submit">Login</Button>
           </Form>
           <p>Forgot password ? </p>

935353464c482198a4f1f0b595136e1d734f9369
konrada111 <konrada111@gmail.com>
2021-04-20 16:23:35 +0200

    "Changed Employees list fixes"

--- client/src/Views/AdminPanel.js
+++ client/src/Views/AdminPanel.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Sidebar from '../components/molecules/AdminSidebar/AdminSidebar';
 import Cabinets from 'components/organism/Cabinets/Cabinets';
-
 import styled from 'styled-components';
 import ProtectedRoute from 'ProtectedRoute/ProtectedRoute';
 import Services from 'components/organism/Services/Services';
+import EmployeesList from '../components/organism/EmployeesList/EmployeesList';
 
 const AdminPanel = () => {
   return (
     <Wrapper>
       <Sidebar />
       <ProtectedRoute exact path="/admin-panel/cabinets" component={Cabinets} isAuth={'admin'} />
+      <ProtectedRoute exact path="/admin-panel/specialists" component={EmployeesList} isAuth={'admin'} />
       <ProtectedRoute exact path="/admin-panel/services" component={Services} isAuth={'admin'} />
     </Wrapper>
   );
--- client/src/components/organism/EmployeesList/EmployeesList.js
+++ client/src/components/organism/EmployeesList/EmployeesList.js
@@ -1,80 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useSelector } from 'react-redux';
-import { selectUserToken } from '../../../features/appSlice';
 import FormField from '../../molecules/FormField/FormField';
 import SelectField from '../../molecules/SelectField/SelectField';
 import { Wrapper, Button, TableWrapper, Header, MainWrapper, EmployeesTable, PaginationWrapper } from './EmployeesList.style';
 import Employee from '../../molecules/Employee/Employee';
 import Pagination from '../../molecules/Pagination/Pagination';
+import useSpecialistForm from '../../../hooks/useSpecialistForm';
 
-const initialFormState = {
-  firstName: '',
-  lastName: '',
-  profession: '',
-  cabinet_id: '',
-};
 const EmployeesList = () => {
-  const [formValues, setFormValues] = useState(initialFormState);
-  const [employees, setEmployees] = useState([]);
-  const [cabinets, setCabinets] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(4);
-
-  const token = useSelector(selectUserToken);
-
-  // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentEmployees = employees.slice(indexOfFirstPost, indexOfLastPost);
-
-  // Change page
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const config = {
-    headers: { Authorization: `Bearer ${token}` },
-  };
-
-  useEffect(() => {
-    const fetchEmployees = async () => {
-      await axios.get('http://127.0.0.1:8000/api/employees').then((response) => setEmployees(response.data.employees));
-    };
-    fetchEmployees();
-  }, [isLoading]);
-
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/cabinets').then((response) => setCabinets(response.data.cabinets));
-  }, []);
-
-  const handleInputChange = (event) => {
-    setFormValues({
-      ...formValues,
-      [event.target.name]: event.target.value,
-    });
-  };
-  const handleDelete = (id) => {
-    setIsLoading((prevState) => !prevState);
-    axios
-      .delete(`http://127.0.0.1:8000/api/employee/${id}`)
-      .then(() => {
-        setIsLoading((prevState) => !prevState);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsLoading((prevState) => !prevState);
-    axios
-      .post('http://127.0.0.1:8000/api/employee', formValues, config)
-      .then((response) => {
-        setIsLoading((prevState) => !prevState);
-      })
-      .catch((err) => console.log(err));
-  };
+  const { handleSubmit, handleDelete, handleInputChange, paginate, currentEmployees, postsPerPage, cabinets, employees, formValues } = useSpecialistForm();
 
   return (
     <MainWrapper>
